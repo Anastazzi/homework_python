cd37a7f61f629bc4548a64a820fa896e5e04a866
depplum <as3662223@gmail.com>
2021-09-27 16:39:47 +0300

    дз за 20.09

---
+++ 20.09/homework.py
@@ -0,0 +1,13 @@
+set_1 = {1, 2, 3, 4, 5, 6, 7, 6, 6 ,5, 5}
+set_2 = {5, 6, 7, 8, 9, 0}
+set_11 = [1, 2, 3, 4, 5, 6]
+set_22 = [5, 6, 7, 8, 9, 0]
+
+def unique(spis):
+    return list(set(spis))
+
+def intersec(set1, set2):
+    return list(set(set1).intersection(set(set2)))
+
+print(unique(set_1))
+print(intersec(set_11, set_22))
